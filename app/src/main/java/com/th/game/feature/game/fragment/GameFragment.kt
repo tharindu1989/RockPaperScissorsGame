22ba6c24d7019802391beb310baa4e78d1e17c96
tharindu1989 <tharindupro89@gmail.com>
2019-08-18 12:11:13 +0800

    Added View Model and observers

--- app/src/main/java/com/th/game/feature/game/fragment/GameFragment.kt
+++ app/src/main/java/com/th/game/feature/game/fragment/GameFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.th.game.R
 import com.th.game.feature.base.BaseFragment
+import com.th.game.feature.game.viewmodel.GameViewModel
+import kotlinx.android.synthetic.main.fragment_game.*
 
 /**
  * Created By Tharindu on 8/18/2019
@@ -17,7 +21,42 @@ class GameFragment : BaseFragment() {
         const val TAG = "GameFragment"
     }
 
+    private lateinit var viewModel: GameViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initLayout()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel = activity?.let {
+            ViewModelProviders.of(it).get(GameViewModel::class.java)
+        } ?: throw Exception("In valid Activity")
+
+        setObservers()
+
+        getData()
+    }
+
+    private fun initLayout() {
+
+    }
+
+    private fun setObservers() {
+
+        viewModel.getCount().observe(this, Observer {
+            countTxt.text = "COUNT $it"
+        })
+    }
+
+    private fun getData() {
+
     }
 }
--- app/src/main/java/com/th/game/feature/game/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/th/game/feature/game/viewmodel/GameViewModel.kt
@@ -1,11 +1,46 @@
 package com.th.game.feature.game.viewmodel
 
+import android.os.CountDownTimer
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.th.game.core.util.ResultMode
 
 /**
  * Created By Tharindu on 8/18/2019
  *
  */
-class GameViewModel : ViewModel(){
+class GameViewModel : ViewModel() {
 
+    val oneSecond: Long = 1000
+    val totalTimeToExpire = oneSecond * 5
+
+    private val countDown: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>().also {
+            startCounter()
+        }
+    }
+
+    private val result: MutableLiveData<ResultMode> by lazy {
+        MutableLiveData<ResultMode>()
+    }
+
+    private fun startCounter() {
+        val timer = object : CountDownTimer(totalTimeToExpire, oneSecond) {
+            override fun onFinish() {
+                checkResult()
+            }
+
+            override fun onTick(p0: Long) {
+                countDown.value = (p0 / oneSecond).toInt()
+            }
+        }
+        timer.start()
+    }
+
+    private fun checkResult() {
+
+    }
+
+
+    fun getCount(): MutableLiveData<Int> = countDown
 }
